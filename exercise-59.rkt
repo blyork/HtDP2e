9e6a3cc9bda94e2d38a60a23c0914f461c372099
Benjamin York <benjaminyork@gmail.com>
2014-12-19 19:14:16 -0600

    Added exercise 59.

---
+++ exercise-59.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-59) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "htdp") (lib "universe.rkt" "teachpack" "2htdp")))))
+(define (distance-to-0 a-posn)
+  (sqrt
+   (+ (sqr (posn-x a-posn))
+      (sqr (posn-y a-posn)))))
+
+(distance-to-0 (make-posn 3 4))
+(distance-to-0 (make-posn 6 (* 2 4)))
+(+ (distance-to-0 (make-posn 12 5)) 10)
